28724971cfcbeb2beb0b794d29f33b226c52d992
divyansh48 <166692812+divyansh48@users.noreply.github.com>
2024-04-13 11:25:24 +0530

    Add files via upload

---
+++ inputs.java
@@ -0,0 +1,16 @@
+package com.divyansh;
+
+import java.util.Scanner;
+
+public class inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your rollno : ");
+        int rollno = input.nextInt();
+        System.out.println("Your rollno is " + rollno);
+
+
+
+
+    }
+}
---
+++ primitivedatatypes.java
@@ -0,0 +1,17 @@
+package com.divyansh;
+
+public class primitivedatatypes {
+    public static void main(String[] args) {
+        System.out.println("these are some datatypes");
+
+        int rollno = 32;
+        float marks = 45.62f;
+
+        double largedecimalnumbers = 8739884.3467236;
+        long largeintegernumbers = 683487263868L;
+        boolean check = true;
+
+
+
+    }
+}
